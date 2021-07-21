bbbb69a96591cf302931494bd194fa17b4b3c796
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-21 12:48:42 +0200

    setting up the function

--- cachematrix.R
+++ cachematrix.R
@@ -22,12 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-#' Compute and cache the inverse of a matrix
-#' @param x the result of a previous makeCacheMatrix call
-#' @param ... additional arguments to pass to solve function
-#' examples
-#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
-#' cacheSolve(x)
+
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
